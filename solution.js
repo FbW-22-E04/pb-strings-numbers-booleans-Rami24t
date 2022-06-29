71a002675a8631ec921c51615433414e2a7057be
Rami <103028944+Rami24t@users.noreply.github.com>
2022-06-29 14:31:27 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,30 @@
+// My Solution:
 
+//    Declare a variable named str and assign a string as its value. Make sure the string is at least 6 characters long. Print the variable to the console.
+
+  str = "Rami Rami";
+console.log(str);
+//  Access the 2nd and 6th elements of the string using index notation. Print the elements to the console.
+console.log(str[1], str[5]);
+
+  //  Declare a variable called firstName and assign a string as its value.
+firstName = "Rami";
+//    Declare a variable called age and assign a number as its value.
+age=36;
+ //   Declare a variable called year and assign a number as its value.
+year = 2022;
+//    Using your declared variables, print a string to the console. For example, "Basem is 37 in 2020".
+console.log(firstName, "is", age, "in", 2020);
+//    Declare a variable called isMarried and assign it a boolean value.
+isMarried = false;
+//    Print a string to the console, indicating whether or not your person is married. For example, "Basem is married: true"
+console.log(firstName, "is married:", isMarried);
+//    Declare a variable called programmingLanguage and assign "JavaScript" as its value. Declare a variable called isFun and give it a value of true
+//. Print "JavaScript is fun: true" to the console, using your declared variables.
+programmingLanguage = "Javascript";
+isFun = true;
+console.log("JavaScript is fun:", isFun);
+//    Print the J and S of "JavaScript" from the variable programmingLanguage to the console.
+console.log(programminLanguage[0], programmingLanguage[1]);
+// Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length());
